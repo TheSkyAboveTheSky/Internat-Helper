7f484ce9d86ea8feae5920d075f7fb30d406571e
oussamarhouch <oussamarhouch@gmail.com>
2023-04-27 16:11:53 +0100

    siu

--- server/src/main/java/com/example/server/repository/UserRepository.java
+++ server/src/main/java/com/example/server/repository/UserRepository.java
@@ -6,12 +6,8 @@ import com.example.server.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByUsername(String username);
-<<<<<<< HEAD:server/src/main/java/com/example/server/repository/UserRepository.java
 
     Boolean existsByUsername(String username);
 
     Boolean existsByEmail(String email);
 }
-=======
-}
->>>>>>> origin:server/src/main/java/com/example/demo/repository/UserRepository.java
