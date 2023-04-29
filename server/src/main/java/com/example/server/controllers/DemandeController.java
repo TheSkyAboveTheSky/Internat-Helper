25c56aaf740c6fa8d2c44844e612f002dfb6a8e6
TheSkyAboveTheSky <94720343+TheSkyAboveTheSky@users.noreply.github.com>
2023-04-29 02:06:42 +0000

    test demandes

--- server/src/main/java/com/example/server/controllers/DemandeController.java
+++ server/src/main/java/com/example/server/controllers/DemandeController.java
@@ -4,21 +4,18 @@ import com.example.server.models.Demande;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.server.services.DemandeService;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/demandes")
 public class DemandeController {
     @Autowired
     private DemandeService demandeService;
     @GetMapping("")
-    public ResponseEntity<List<Demande>> getDemandes(){
+    public ResponseEntity<List<Demande>> allAccess(){
         return new ResponseEntity<>( demandeService.getDemandes(), HttpStatus.OK);
 
 
--- server/src/main/java/com/example/server/services/DemandeService.java
+++ server/src/main/java/com/example/server/services/DemandeService.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Service
 public class DemandeService {
-    static private ArrayList<Demande> demandes= new ArrayList<>(Arrays.asList(
+    static private final ArrayList<Demande> demandes= new ArrayList<>(Arrays.asList(
             new Demande(1,"B32","plomberie", "probleme d'eau",Demande.Etat.En_cours),
             new Demande(2,"E321","Electricité","Une LED ne marche plus", Demande.Etat.Traité),
             new Demande(3,"D56","Porte","Ma porte ne se ferme pas correctement", Demande.Etat.Non_traité)
