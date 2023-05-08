1f5c9ab0f3ecd969ba6f56f1a04453f5e57f896b
oussamarhouch <oussamarhouch@gmail.com>
2023-05-08 19:29:54 +0100

    authentification

--- client/src/app/Material.Module.ts
+++ client/src/app/Material.Module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  exports : [MatTableModule,MatIconModule,MatPaginatorModule,MatFormFieldModule]
+  exports: [
+    MatTableModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+  ],
 })
 export class MaterialModule {}
