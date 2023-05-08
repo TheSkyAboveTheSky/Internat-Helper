b0b0cb2b5cbe9ad332978b2381fff2c0010273e3
oussamarhouch <oussamarhouch@gmail.com>
2023-05-08 23:21:53 +0100

    allUsers

--- server/src/main/java/com/example/server/repository/UserRepository.java
+++ server/src/main/java/com/example/server/repository/UserRepository.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByUsername(String username);
-    Optional<User> findById(String Id);
 
     Optional<User> findById(String Id);
 
