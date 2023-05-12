a400558403a4380e928ee187e5d5c188b4e84480
TheSkyAboveTheSky <94720343+TheSkyAboveTheSky@users.noreply.github.com>
2023-05-12 02:41:56 +0000

    Revert "add some folders"
    This reverts commit 17dc2522bc5a57bd8c0d2928af2c2f17742bda81.

---
+++ src/app/app.module.ts
@@ -0,0 +1,64 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BoardAdminComponent } from './components/board-admin/board-admin.component';
+import { BoardModeratorComponent } from './components/board-moderator/board-moderator.component';
+import { BoardUserComponent } from './components/board-user/board-user.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { EditProfilComponent } from './components/edit_profil/edit_profil.component';
+import  {EditImageComponent} from "./components/edit_image/edit_image.component";
+import { AddNewProblemComponent } from './components/add-new-problem/add-new-problem.component';
+import {MatInputModule} from "@angular/material/input";
+import { ProblemService } from './services/problem/problem.service';
+import { ShowProblemDetailsComponent } from './components/show-problem-details/show-problem-details.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from "@angular/material/button";
+import {MaterialModule} from "./Material.Module";
+import { ShowProblemImageDialogComponent } from './components/show-problem-image-dialog/show-problem-image-dialog.component';
+import {MatDialogModule} from '@angular/material/dialog';
+@NgModule({
+  declarations: [
+    AppComponent,
+    BoardAdminComponent,
+    BoardModeratorComponent,
+    BoardUserComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    EditProfilComponent,
+    EditImageComponent,
+    AddNewProblemComponent,
+    ShowProblemDetailsComponent,
+    ShowProblemImageDialogComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    UserListComponent,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatInputModule,
+    MatGridListModule,
+    MatTableModule,
+    MatButtonModule,
+    MaterialModule,
+    MatDialogModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
