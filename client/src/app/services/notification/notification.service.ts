8b984a9118c6f79ec72df7798a03d304bcf0fc7b
TheSkyAboveTheSky <94720343+TheSkyAboveTheSky@users.noreply.github.com>
2023-05-21 19:15:01 +0000

    fix some colors

--- client/src/app/services/notification/notification.service.ts
+++ client/src/app/services/notification/notification.service.ts
@@ -1,27 +1,31 @@
-import { Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+
 @Injectable({
-  providedIn: 'root',})
+  providedIn: 'root',
+})
 export class NotificationService {
   constructor(private snackbar: MatSnackBar) {}
 
   config: MatSnackBarConfig = {
-    duration: 90000,
+    duration: 2000,
     verticalPosition: 'top',
     horizontalPosition: 'right',
-    panelClass:'notification'
+    panelClass:['notification']
   };
 
   success(msg: any) {
-    this.config['panelClass'] = ['notification', 'success'];
-    this.snackbar.open(msg, '', this.config);
+    this.config.panelClass = ['notification', 'success'];
+    this.snackbar.open(msg, 'Close', this.config);
   }
+
   error(msg: any) {
-    this.config['panelClass'] = ['notification', 'error']; 
-    this.snackbar.open(msg, '', this.config);
+    this.config.panelClass = ['notification', 'error'];
+    this.snackbar.open(msg, 'Close', this.config);
   }
-  warn(msg:any){
-    this.config['panelClass'] = ['notification', 'warn']; 
-    this.snackbar.open(msg, '', this.config);
+
+  warn(msg: any) {
+    this.config.panelClass = ['notification', 'warn'];
+    this.snackbar.open(msg, 'Close', this.config);
   }
 }
